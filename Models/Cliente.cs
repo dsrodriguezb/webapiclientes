762675b0adc539ec9372e2dcd44fb31ccb0aec01
dsrodriguezb <davidsebas995@gmail.com>
2024-09-30 15:11:48 -0500

    Repositorio sencillo con net core 8 y entity framework

---
+++ Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApiClientes.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Address { get; set; }
+        public required int Age { get; set; }
+        public int Phone { get; set; }
+        public DateOnly Fecha { get; set; }
+
+    }
+}
